c2165f54f84deee5b5609bfc9282cd6214e4279d
Esther van Enckevort <esther@vanenckevort.net>
2020-06-01 10:29:26 +0200

    Initial Commit

---
+++ Sudoku/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Sudoku
+//
+//  Created by Esther van Enckevort on 01-06-2020.
+//  Copyright © 2020 All things digital. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
